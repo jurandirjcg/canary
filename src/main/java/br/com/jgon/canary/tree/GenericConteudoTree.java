acc037ccd2b2c5ba8d01c9466c418ac90ccf8f72
Jurandir C. Gonçalves <juragoncalves@gmail.com>
2019-11-23 20:43:23 -0300

    formatacao

--- src/main/java/br/com/jgon/canary/tree/GenericConteudoTree.java
+++ src/main/java/br/com/jgon/canary/tree/GenericConteudoTree.java
@@ -16,72 +16,75 @@ package br.com.jgon.canary.tree;
 import java.io.Serializable;
 
 /**
- * Classe abastrata para utilização no conteúdo dos nodes da arvore {@link GenericTree}
+ * Classe abastrata para utilização no conteúdo dos nodes da arvore
+ * {@link GenericTree}
  * 
  * @author Jurandir C. Goncalves
  * 
  * @version 1.0
  *
  */
-public abstract class GenericConteudoTree implements Serializable{
+public abstract class GenericConteudoTree implements Serializable {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	/**
-	 * Identificacao do conteúdo
-	 */
-	@FieldGenericTree(mapearAtributo=false)
-	private String label;
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    /**
+     * Identificacao do conteúdo
+     */
+    @FieldGenericTree(mapearAtributo = false)
+    private String label;
 
-	public GenericConteudoTree(String label){
-		this.label = label;
-	}
-	
-	public GenericConteudoTree(){
-		
-	}
-	
-	/**
-	 * Identificacao do conteúdo
-	 * @return {@link String}
-	 */
-	public String getLabel() {
-		return label;
-	}
+    public GenericConteudoTree(String label) {
+        this.label = label;
+    }
 
-	/**
-	 * Identificacao do conteúdo
-	 * @param label - identificação
-	 */
-	public void setLabel(String label) {
-		this.label = label;
-	}
+    public GenericConteudoTree() {
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((label == null) ? 0 : label.hashCode());
-		return result;
-	}
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		GenericConteudoTree other = (GenericConteudoTree) obj;
-		if (label == null) {
-			if (other.label != null)
-				return false;
-		} else if (!label.equals(other.label))
-			return false;
-		return true;
-	}	
+    /**
+     * Identificacao do conteúdo
+     * 
+     * @return {@link String}
+     */
+    public String getLabel() {
+        return label;
+    }
+
+    /**
+     * Identificacao do conteúdo
+     * 
+     * @param label - identificação
+     */
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((label == null) ? 0 : label.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        GenericConteudoTree other = (GenericConteudoTree) obj;
+        if (label == null) {
+            if (other.label != null)
+                return false;
+        } else if (!label.equals(other.label))
+            return false;
+        return true;
+    }
 
 }
